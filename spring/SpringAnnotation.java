0d4887758a06237fb1babb1263949443fdbf1d43
Nareshbhai Mahto <82165555+NareshMahto@users.noreply.github.com>
2026-06-09 17:30:21 +0530

    Create SpringAnnotation.java

---
+++ spring/SpringAnnotation.java
@@ -0,0 +1,7 @@
+/*
+What is an Annotation in Spring?
+--------------------------------
+An annotation is a special marker that provides metadata (information about the code) to the Spring Framework. 
+Spring uses annotations to configure and manage beans, dependencies, web requests, transactions, and more 
+without requiring extensive XML configuration.
+*/
